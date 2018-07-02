daba917a8d24092e9f92f5ec2c1332277edb515a
maciabe <maciabe@hotmail.com>
2018-07-03 00:28:30 +0200

    menus wip

--- utils.js
+++ utils.js
@@ -1,4 +1,10 @@
 var menu = document.querySelector('.nav-menu-mobile-drop')
+
 menu.addEventListener('click', function(){
   document.querySelector('.nav-menu-mobile-links').classList.add('nav-menu-mobile-links-visible')
+})
+
+var links = document.querySelector('.nav-menu-mobile-links')
+links.addEventListener('click', function(){
+  document.querySelector('.nav-menu-mobile-links-visible').classList.remove('nav-menu-mobile-links-visible')
 })
