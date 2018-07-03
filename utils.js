b57de27f1d71d3415867c3ea2fd597ef39069ded
maciabe <maciabe@hotmail.com>
2018-07-03 15:31:16 +0200

    cleanup & styling

--- utils.js
+++ utils.js
@@ -1,14 +1,30 @@
+
 var menu = document.querySelector('.nav-menu-mobile-drop')
 menu.addEventListener('click', function(){
   document.querySelector('.nav-menu-mobile-links').classList.add('nav-menu-mobile-links-visible')
 })
+
+var menuIcon = document.querySelector('.nav-menu-mobile-icon')
+menuIcon.addEventListener('click', function(){
+  document.querySelector('.nav-menu-mobile-links').classList.add('nav-menu-mobile-links-visible')
+})
+
+
+
+
 
 var links = document.querySelector('.nav-menu-mobile-links')
 links.addEventListener('click', function(){
   document.querySelector('.nav-menu-mobile-links-visible').classList.remove('nav-menu-mobile-links-visible')
 })
 
-var home = document.querySelector('.nav-home')
-home.addEventListener('click', function(){
+var homeText = document.querySelector('.nav-home-text')
+homeText.addEventListener('click', function(){
   document.querySelector('.nav-menu-mobile-links-visible').classList.remove('nav-menu-mobile-links-visible')
 })
+
+var homeIcon = document.querySelector('.nav-home-picture')
+homeIcon.addEventListener('click', function(){
+  document.querySelector('.nav-menu-mobile-links-visible').classList.remove('nav-menu-mobile-links-visible')
+})
+
