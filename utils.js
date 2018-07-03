d93e7df03d988ca6b504eb02770defc718a71b45
maciabe <maciabe@hotmail.com>
2018-07-03 02:26:24 +0200

    rwd tryouts

--- utils.js
+++ utils.js
@@ -1,10 +1,14 @@
 var menu = document.querySelector('.nav-menu-mobile-drop')
-
 menu.addEventListener('click', function(){
   document.querySelector('.nav-menu-mobile-links').classList.add('nav-menu-mobile-links-visible')
 })
 
 var links = document.querySelector('.nav-menu-mobile-links')
 links.addEventListener('click', function(){
+  document.querySelector('.nav-menu-mobile-links-visible').classList.remove('nav-menu-mobile-links-visible')
+})
+
+var home = document.querySelector('.nav-home')
+home.addEventListener('click', function(){
   document.querySelector('.nav-menu-mobile-links-visible').classList.remove('nav-menu-mobile-links-visible')
 })
